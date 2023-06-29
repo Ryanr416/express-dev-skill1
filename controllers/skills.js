ba1285c205287c1de9ef684fe065d2c2b0d0b578
ryanr416 <ryanrichardson0993@gmail.com>
2023-06-29 11:03:37 -0400

    added create

--- controllers/skills.js
+++ controllers/skills.js
@@ -5,8 +5,18 @@ const Skill = require('../models/skill');
 module.exports = {
     index,
     show, 
-    new: newSkills
+    new: newSkills,
+    create
 };
+
+
+//create always redirects to either the show page or index page
+function create(req, res) {
+    res.redirect('/Skills') // tells the client to make a git request to /skills
+}
+
+
+
 function newSkills(req, res){
     res.render('Skills/new')
 }
--- routes/skills.js
+++ routes/skills.js
@@ -7,10 +7,17 @@ const skillsCtrl = require('../controllers/skills');
 // GET request to /skills
 router.get('/', skillsCtrl.index);
 
+// submit the request
+router.post('/', skillsCtrl.create)
+
+
 // Get request for creating
 router.get('/new', skillsCtrl.new);
+
 // GET request to /skills/:id
 
 router.get('/:id', skillsCtrl.show);
+
+
 
 module.exports = router;
